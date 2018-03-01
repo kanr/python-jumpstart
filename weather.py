735f5fac4dadd837d57749d53888f62f42eee79f
Connor <connor.aitken@gmail.com>
2018-02-28 23:58:40 -0800

    starting weather client app

---
+++ weather.py
@@ -0,0 +1,16 @@
+#weather client
+# Connor aitken
+
+
+def print_header():
+    print('_______________________________')
+    print('_______Weather Client__________')
+    print('_______________________________')
+    print()
+
+
+def main():
+    print_header()
+
+if __name__=='__main__':
+    main()
